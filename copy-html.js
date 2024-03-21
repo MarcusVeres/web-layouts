8230fb0c0431f4521f659453896d1145e463a654
Marcus <marcus.veres@gmail.com>
2024-03-21 15:21:00 -0400

    Adds new website - make-it-and-go

--- copy-html.js
+++ copy-html.js
@@ -1,20 +1,21 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 function copyDirectory(src, dest) {
-    fs.readdirSync(src).forEach(item => {
-        const srcItem = path.join(src, item);
-        const destItem = path.join(dest, item);
-        const stat = fs.statSync(srcItem);
+  fs.readdirSync(src).forEach((item) => {
+    const srcItem = path.join(src, item);
+    const destItem = path.join(dest, item);
+    const stat = fs.statSync(srcItem);
 
-        if (stat.isDirectory()) {
-            fs.mkdirSync(destItem, { recursive: true });
-            copyDirectory(srcItem, destItem);
-        } else {
-            fs.copyFileSync(srcItem, destItem);
-            console.log(`Copied file: ${srcItem}`);
-        }
-    });
+    if (stat.isDirectory()) {
+      fs.mkdirSync(destItem, { recursive: true });
+      copyDirectory(srcItem, destItem);
+    } else {
+      fs.copyFileSync(srcItem, destItem);
+      console.log(`Copied file: ${srcItem}`);
+    }
+  });
 }
 
-copyDirectory(path.join(__dirname, 'src/pages'), path.join(__dirname, 'docs'));
+copyDirectory(path.join(__dirname, "src/assets"), path.join(__dirname, "docs"));
+copyDirectory(path.join(__dirname, "src/pages"), path.join(__dirname, "docs"));
